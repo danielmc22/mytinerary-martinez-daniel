86477c860551a18fef9e10fd41de179fc60f8fc3
Juan Carlos Lopez Ortiz <danielfm.c222@hotmail.com>
2022-02-16 16:39:01 -0500

    commit inicial, creación de archivos

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Aprendiendo React Clase 1
+        </a>
+        <h2 style="color: green">iniciando React</h2>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
